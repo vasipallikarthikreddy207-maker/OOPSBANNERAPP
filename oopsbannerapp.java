0980d385ef5ab3cc6317fba9e15a57f8dca94ef9
vasipallikarthikreddy207-maker <vasipallikarthikreddy207@gmail.com>
2026-03-26 09:42:32 +0530

    Completed UC2:Printed OOPS Banner

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -1,6 +1,11 @@
 public class oopsbannerapp {
-    public static void main(String[] args)
-    {
-        System.out.println("OOPS");
+    public static void main(String[] args){
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "  + "   ***** ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "  + " **      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "  + "**       ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "  + "  *****  ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "  + "      ** ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "  + " **   ** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "  + "  *****  ");
     }
 }
